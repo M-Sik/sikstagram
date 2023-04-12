5ed651f8c45074103350c235bda4b55b3496c43e
MyeongSik <77043973+M-Sik@users.noreply.github.com>
2023-04-12 13:29:23 +0900

    Dev (#1)
    * [feat] sanity 스키마 추가
    
    * [design] sanity스튜디오 데이터 리스트 ui 커스텀

---
+++ sanity-studio/schemas/user.js
@@ -0,0 +1,73 @@
+//  title: 세니티 스튜디오에서 볼 이름
+// name: 백엔드에서 쓸 키 이름
+export default {
+    title: 'User',
+    name: 'user',
+    type: 'document',
+    fields: [
+        // 사용자 아이디
+        {
+            title: 'Username',
+            name: 'username',
+            type: 'string',
+        },
+        // 사용자 이름
+        {
+            title: 'Name',
+            name: 'name',
+            type: 'string'
+        },
+        {
+            title: 'Email',
+            name: 'email',
+            type: 'string'
+        },
+        {
+            title: 'Image',
+            name: 'image',
+            type: 'string'
+        },
+        {
+            title: 'Following',
+            name: 'following',
+            type: 'array',
+            of: [
+                {
+                    type: 'reference',
+                    to: [{type: 'user'}]
+                }
+            ],
+            validation: (Rule) => Rule.unique(),
+        },
+        {
+            title: 'Followers',
+            name: 'followers',
+            type: 'array',
+            of: [
+                {
+                    type: 'reference',
+                    to: [{type: 'user'}]
+                }
+            ],
+            validation: (Rule) => Rule.unique(),
+        },
+        {
+            title: 'Bookmarks',
+            name: 'bookmarks',
+            type: 'array',
+            of: [
+                {
+                    type: 'reference',
+                    to: [{type: 'post'}]
+                }
+            ],
+            validation: (Rule) => Rule.unique(),
+        }
+    ],
+    preview: {
+        select: {
+            title: 'name',
+            subtitle: 'username',
+        }
+    }
+}
