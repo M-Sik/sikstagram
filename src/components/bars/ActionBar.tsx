938f9bbbc60f7f789d2c75520f100fcee1fa1086
김명식 <rlaaudtlr233@naver.com>
2023-04-26 15:28:51 +0900

    [feat] 포스트 좋아요 스크랩버튼 토글 기능 구현

--- src/components/bars/ActionBar.tsx
+++ src/components/bars/ActionBar.tsx
@@ -1,7 +1,12 @@
-import React from 'react';
+'use client';
+
+import React, { useState } from 'react';
 import HeartIcon from '../icons/HeartIcon';
 import BookmarkIcon from '../icons/BookmarkIcon';
 import { parseDate } from '@/util/date';
+import ToggleBtn from '../buttons/ToggleBtn';
+import HeartFillIcon from '../icons/HeartFillIcon';
+import BookmarkFillIcon from '../icons/BookmarkFillIcon';
 
 interface IPorps {
   likes: string[];
@@ -11,11 +16,23 @@ interface IPorps {
 }
 
 export default function ActionBar({ likes, username, text, createdAt }: IPorps) {
+  const [liked, setLiked] = useState(false);
+  const [bookmarked, setBookmarked] = useState(false);
   return (
     <>
-      <div className="flex p-2 gap-2">
-        <HeartIcon />
-        <BookmarkIcon />
+      <div className="flex p-2 justify-between">
+        <ToggleBtn
+          toggled={liked}
+          onToggle={setLiked}
+          onIcon={<HeartFillIcon />}
+          offIcon={<HeartIcon />}
+        />
+        <ToggleBtn
+          toggled={bookmarked}
+          onToggle={setBookmarked}
+          onIcon={<BookmarkFillIcon />}
+          offIcon={<BookmarkIcon />}
+        />
       </div>
       <div className="p-2">
         <p className="text-sm font-bold">좋아요 {`${likes?.length ?? 0}`}</p>
---
+++ src/components/buttons/ToggleBtn.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+type Props = {
+  toggled: boolean;
+  onToggle: (toggled: boolean) => void;
+  onIcon: React.ReactNode;
+  offIcon: React.ReactNode;
+};
+
+export default function ToggleBtn({ toggled, onToggle, onIcon, offIcon }: Props) {
+  return <button onClick={() => onToggle(!toggled)}>{toggled ? onIcon : offIcon}</button>;
+}
