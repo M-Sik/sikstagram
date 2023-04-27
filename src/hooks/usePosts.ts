a732b709ba70133764d32b767dc6b77c58ccc80a
김명식 <rlaaudtlr233@naver.com>
2023-04-27 15:16:42 +0900

    [refactor] 게시글 좋아요시 useSWR에 있는 mutate를 사용하며, optimisticData를 활용하여 비동기 처리 결과가 오기 전에 UI에 좋아요 결과 반영 - 만약 비동기 처리가 실패하였다면 mutate함수 하위 rollbackOnError 옵션을 true로 설정 하였기에 기존 UI로 롤백됨

--- src/hooks/usePosts.ts
+++ src/hooks/usePosts.ts
@@ -1,17 +1,39 @@
 import useSWR, { useSWRConfig } from 'swr';
 import { SimplePost } from '@/types/types';
 
-export default function usePosts() {
-  const { data: posts, isLoading, error } = useSWR<SimplePost[]>('/api/posts');
+async function updateLike(id: string, like: boolean) {
+  // tip) get요청은 캐싱이 필요할 수 있기에 swr사용, 이외 put. patch, delete등 수정하는 요청은 fetch
+  return fetch('/api/likes', {
+    method: 'PUT',
+    body: JSON.stringify({ id, like }),
+  }).then((res) => res.json());
+}
 
+export default function usePosts() {
   // tip) 좋아요로 인해 포스트에 정보가 바뀌었으므로 포스트 정보 갱신해줌 ex) mutate('/api/posts')
-  const { mutate } = useSWRConfig();
+  const { data: posts, isLoading, error, mutate } = useSWR<SimplePost[]>('/api/posts');
+
   const setLike = (post: SimplePost, username: string, like: boolean) => {
-    // tip) get요청은 캐싱이 필요할 수 있기에 swr사용, 이외 put. patch, delete등 수정하는 요청은 fetch
-    fetch('/api/likes', {
-      method: 'PUT',
-      body: JSON.stringify({ id: post.id, like }),
-    }).then(() => mutate('/api/posts'));
+    const newPost = {
+      ...post,
+      likes: like ? [...post.likes, username] : post.likes.filter((item) => item !== username),
+    };
+    const newPosts = posts?.map((p) => (p.id === post.id ? newPost : p));
+
+    return mutate(updateLike(post.id, like), {
+      // tip) 비동기 통신 후 포스트 업데이트 결과를 mutate하면 느리게 반영되서
+      // useSWR에서 mutate 선언 후 가져와서 사용한다.
+      // optimisticData 옵션은 비동기 요청을 하며 바로 UI를 수정하라는 옵션
+      optimisticData: newPosts,
+      // 원래같으면 비동기 처리된 결과로 업데이트를 해주지만
+      // optimisticData로 요청과 동시에 ui를 수정할 것이기 때문에
+      // updateLike에서 반환된 값을 mutate에 덮어씌우지 않을것이기에 populateCache는 false
+      // populateCache는 false로 해야한다.
+      populateCache: false,
+      revalidate: false,
+      // 비동기 처리를 만약 실패할경우 기존 ui로 롤백함 즉, optimisticData 적용을 취소
+      rollbackOnError: true,
+    });
   };
 
   return { posts, isLoading, error, setLike };
